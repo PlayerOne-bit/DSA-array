4dd1597e21f1291b0596b9db23456ad8312ec414
PlayerOne-bit <codexalpha24@gmail.com>
2025-08-20 07:07:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ button.addEventListener('click',()=>{
 black.addEventListener("click",()=>{
     sidebar.classList.remove("show");
     black.classList.remove("show");
-});
-
+});
+let once=true;
 function submit(){
+if(once){
     let score=0;
     const questions = ['choice1','choice2','choice3','choice4','choice5','choice6','choice7','choice8','choice9','choice0'];
     questions.forEach(name=>{
@@ -29,7 +30,7 @@ function submit(){
     const essay2=document.querySelector(".essay2").value;
     congrats.innerHTML=`Congrats! ${(name==="")?"Guest":name} from ${(course==="")?"Nowhere":course}!`;
     result.innerHTML=`You got a score of <b style='font-size:500%; color:lightgreen;'>${score}/10</b><br>But quiz checkers will still check your answers from essay:<br>11-12. ${(essay1==="")?"It's blank LOLOLOLOLOL!!!!!":essay1}<br>13-15. ${(essay2==="")?"Wow you didn't even tried to do your best ha?":essay2}`;
-
-
+once=false;
+}
 
 }
